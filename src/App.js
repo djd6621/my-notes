bdb07ed850a15503a9b9c41046a7cbc1da2285ea
Darek <darekdunbar@gmail.com>
2019-11-12 17:53:57 -0500

    Button Code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-      <div><h1>Hello, Darek!</h1></div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        isLoggedIn: prevState.isLoggedIn ? false : true
+      }
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          <h1>{this.state.isLoggedIn ? "You are logged in!" : "You are logged out!"}</h1>
+          <button type="button" onClick={this.handleClick}>{this.state.isLoggedIn ? "Log Out!" : "Log In!"} </button>
+        </div>
+    )
+
+  }
+
 }
 
 export default App;
