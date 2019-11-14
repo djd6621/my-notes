c67b99318c1a8011c4dacb401159fef0e974b86e
Darek <darekdunbar@gmail.com>
2019-11-14 12:39:59 -0500

    prac

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
   render() {
     return (
         <div>
+          <form>
+            <input type="text" placeholder="First Name"/>
+            <input type="text" placeholder="First Name"/>
+          </form>
           <h1>{this.state.isLoggedIn ? "You are logged in!" : "You are logged out!"}</h1>
           <button type="button" onClick={this.handleClick}>{this.state.isLoggedIn ? "Log Out!" : "Log In!"} </button>
         </div>
@@ -29,4 +33,39 @@ class App extends Component {
 
 }
 
-export default App;
+/*export default App;
+
+
+
+import React, {Component} from "react"
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          <input type="text" name="firstName" placeholder="first name" onChange={this.handleChange}/>
+          <br/>
+          <input type="text" name="lastName" placeholder="last name" onChange={this.handleChange}/>
+          <h1>{this.state.firstName} {this.state.lastName}</h1>
+        </div>
+    )
+  }
+}
+
+export default App*/
+
